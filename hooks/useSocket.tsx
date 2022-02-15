fe2eeb61bee38e8c5b5034197a3e28205f21c73e
janper231 <jeancarloss659@gmail.com>
2022-02-15 00:00:34 -0500

    feat: update transports method

--- hooks/useSocket.tsx
+++ hooks/useSocket.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 
 export const useSocket = () => {
-  const socket = io()
+  const socket = io({ transports: ['websocket', 'polling', 'flashsocket'] })
 
   useEffect(() => {
     fetch('/api/socket')
